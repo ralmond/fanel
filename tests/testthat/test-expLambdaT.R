12a748144dea4e81638b7ada542834e523548fc1
Russell G Almond <russell.g.almond@gmail.com>
2026-01-08 19:25:34 -0500

    Backfill expLambdaT test.

--- tests/testthat/test-expLambdaT.R
+++ tests/testthat/test-expLambdaT.R
@@ -1,5 +1,12 @@
 test_that("expLambdaT {expLambdaT}", {
-  ## expLambdaT(Lambda, nmoments = 10)
+
+  I <- diag(3)
+  ee <- diag(rep(exp(1),3))
+  expect_equal(expLambdaT(I,25),ee,
+               tolerance=.0001)
+
+  lam1 <- matrix(c(-.25,.25,0, 0.01,-.26,.25, 0,0.01,-.01),3,3,byrow=TRUE)
+  expect_equal(expLambdaT(lam1,1),(I+lam1/2)%*%(I+lam1/2))
 })
 
 
